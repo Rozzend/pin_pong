46bfdc65913a39504136ccabfc8e30ad560f7e95
Rozzend <82720150+Rozzend@users.noreply.github.com>
2021-05-06 13:40:14 +0300

    Update main.py
    перемещение спрайтов

--- main.py
+++ main.py
@@ -25,6 +25,21 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 160:
+           self.rect.y += self.speed
+    def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 160:
+           self.rect.y += self.speed
+
+
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
@@ -36,9 +51,12 @@ finish = False
 clock = time.Clock()
 FPS = 100
 
-racket1 = GameSprite('racket.png', 30, 200, 4, 50, 150)
-racket2 = GameSprite('racket.png', 520, 200, 4, 50, 150)
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+speed_x = 3
+speed_y = 3
 
 while game:
    #событие нажатия на кнопку Закрыть
@@ -50,14 +68,12 @@ while game:
    if not finish:
        #обновляем фон
        window.blit(background,(0,0))
+       racket1.update_l()
+       racket2.update_r()
 
        racket1.reset()
        racket2.reset()
        ball.reset()
 
-       display.update()
-
-   else:
-       finish = False
-
-   time.delay(25)
+   display.update()
+   clock.tick(FPS)

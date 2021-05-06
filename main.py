b3a45f2020ba469d151cb5ba714d43fdb1595317
Rozzend <82720150+Rozzend@users.noreply.github.com>
2021-05-06 13:25:12 +0300

    Update main.py
    на экране появились спрайты

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ max_lost = 99999
 life = 3
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.image = transform.scale(image.load(player_image), (wight, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -25,7 +25,7 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-win_width = 700
+win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 display.set_caption("pin_pong")
@@ -36,24 +36,28 @@ finish = False
 clock = time.Clock()
 FPS = 100
 
+racket1 = GameSprite('racket.png', 30, 200, 4, 50, 150)
+racket2 = GameSprite('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
 while game:
    #событие нажатия на кнопку Закрыть
    for e in event.get():
        if e.type == QUIT:
            game = False
-       #событие нажатия на пробел - спрайт стреляет
-       elif e.type == KEYDOWN:
-           if e.key == K_SPACE:
-               fire_sound.play()
-               player.fire()
+
  #сама игра: действия спрайтов, проверка правил игры, перерисовка
    if not finish:
        #обновляем фон
        window.blit(background,(0,0))
 
+       racket1.reset()
+       racket2.reset()
+       ball.reset()
+
        display.update()
 
    else:
        finish = False
-       
+
    time.delay(25)
